3a44895b8077f6d1628e82e4f6e2f69ef480cf56
Christopher Hernandez <cerberean661@gmail.com>
2019-11-01 10:49:27 -0700

    dark mode testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent } from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('darkmode', () => {
+  let click = false;
+  const { getByText } = render(<App darkmode = {() => click = true} />);
+  const darkMode = getByText(/dark mode/i);
+  fireEvent.click(darkMode);
+  expect(click).toBe(false);
+})
